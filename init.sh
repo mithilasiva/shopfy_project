1e518a29d8d3e3e4b062a13c6de6e22a59e4e19c
Mithila Sivakumar <mithi.scooby@gmail.com>
2018-10-14 10:44:41 -0400

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 export SECRET_KEY_BASE=$(bundle exec rake secret)
 
-bundle exec rake db:create db:migrate
+bundle exec rake db:create
+bundle exec rails db:migrate RAILS_ENV=development
 bundle exec rails server -b 0.0.0.0
